efa324fdcd0bbf990c029734c5feaf73a75b2fa3
Lior <Zwananiluyanda@gmail.com>
2025-09-03 23:01:29 +0200

    Update callbackVersion.ts

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -3,15 +3,15 @@ import https from 'https';
 function fetchData(url: string, callback: (err: any, data?: any) => void) {
   https.get(url, (res) => {
     let data = '';
-    .on('data', chunk => data += chunk);
-    .on('end', () => callback(null, JSON.parse(data)));
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => callback(null, JSON.parse(data)));
   }).on('error', (err) => callback(err));
 }
  
 // Callback hell example
 fetchData('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true', (err, weather) => {
   if (err) return console.error('Weather Error:', err);
-  .log('Weather:', weather.current_weather);
+  console.log('Weather:', weather.current_weather);
 
   fetchData('https://dummyjson.com/posts', (err, news) => {
     if (err) return console.error('News Error:', err);
